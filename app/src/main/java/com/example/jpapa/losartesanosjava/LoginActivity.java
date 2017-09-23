04623895dc62e3a01fce66dd6172f9bcc3290696
jpapa13 <32025521+jpapa13@users.noreply.github.com>
2017-09-23 00:05:11 -0500

    Nueva actividad menu que saluda al usuario por email

--- app/src/main/java/com/example/jpapa/losartesanosjava/LoginActivity.java
+++ app/src/main/java/com/example/jpapa/losartesanosjava/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.jpapa.losartesanosjava;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -42,7 +43,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     /**
      * Id to identity READ_CONTACTS permission request.
      */
-    private static final int REQUEST_READ_CONTACTS = 0;
+    private static final int REQUEST_READ_CONTACTS = 2;
 
     /**
      * A dummy authentication store containing known user names and passwords.
@@ -56,14 +57,18 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      */
     private UserLoginTask mAuthTask = null;
 
+
+    //Credenciales por default
+    private String AdminEmailView = "admin";
+    private String AdminPasswordView = "admin";
     // UI references.
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Set up the login form.
@@ -92,6 +97,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+    }
+    public void entrar(View view) {
+        Intent i = new Intent(this, MainMenuActivity.class );
+        startActivity(i);
     }
 
     private void populateAutoComplete() {
@@ -187,6 +196,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+            Intent intent = new Intent(this, MainMenuActivity.class );
+            intent.putExtra("mEmailView", mEmailView.getText().toString());
+            startActivity(intent);
         }
     }
 
@@ -346,5 +358,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
         }
     }
+
 }
 
